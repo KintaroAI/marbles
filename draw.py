d03d8cec2b11c65c9c84a386681eeedb418db59d
Baraban <baraban@kintaroai.com>
2026-01-20 19:43:50 -0600

    Updated to v0.4

--- draw.py
+++ draw.py
@@ -13,8 +13,8 @@ GRID_WIDTH = 17
 GRID_HEIGHT = 15
 INIT_HEIGHT = 9
 
-SCREEN_WIDTH = (BUBBLE_SIZE + BUBBLE_SPACE // 2) * GRID_WIDTH - BUBBLE_SPACE // 2
-SCREEN_HEIGHT = (BUBBLE_SIZE + BUBBLE_SPACE // 2) * GRID_HEIGHT - BUBBLE_SPACE // 2
+SCREEN_WIDTH = (BUBBLE_SIZE + BUBBLE_SPACE // 2) * GRID_WIDTH
+SCREEN_HEIGHT = (BUBBLE_SIZE + BUBBLE_SPACE // 2) * GRID_HEIGHT
 
 # Colors
 WHITE = (255, 255, 255)
@@ -33,6 +33,27 @@ def load_bubble_image(color):
 
 # Bubble colors
 colors = [WHITE, (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
+
+def get_center(c):
+    return c * (BUBBLE_SIZE + BUBBLE_SPACE//2) + BUBBLE_SIZE // 2 + BUBBLE_SPACE // 2
+
+def neigbour_cells(cell):
+    cx, cy = cell
+    for next_cx, next_cy in [
+        (cx, cy+1),
+        (cx, cy-1),
+        (cx+1, cy),
+        (cx-1, cy),
+        (cx+1, cy+1),
+        (cx+1, cy-1),
+        (cx-1, cy+1),
+        (cx-1, cy-1),
+    ]:
+        if next_cx < 0 or next_cy < 0:
+            continue
+        if next_cx >= GRID_WIDTH or next_cy >= GRID_HEIGHT:
+            continue
+        yield (next_cx, next_cy)
 
 class Board:
     def __init__(self):
@@ -73,12 +94,12 @@ class Board:
         self.colors = colors
         if not self.next_bubble:
             self.create_next_bubble()
-        for y in range(INIT_HEIGHT):
-            for x in range(GRID_WIDTH):
+        for cy in range(INIT_HEIGHT):
+            for cx in range(GRID_WIDTH):
                 color = random.choice(self.colors)
                 bubble = Bubble(
-                    x * (BUBBLE_SIZE + BUBBLE_SPACE//2),
-                    y * (BUBBLE_SIZE + BUBBLE_SPACE//2),
+                    get_center(cx),
+                    get_center(cy),
                     0,
                     0,
                     color)
@@ -90,16 +111,86 @@ class Board:
         bubble = self.current_bubble
         # Check for collision with other bubbles
         # The first False is for not killing the bubble being checked; the second is to use the collide_circle method
-        collided_bubbles = pygame.sprite.spritecollide(bubble, self.bubbles, False, pygame.sprite.collide_circle)
+        collided_bubbles = pygame.sprite.spritecollide(
+            bubble, self.bubbles, False, pygame.sprite.collide_circle)
         if len(collided_bubbles) > 1:  # It always finds itself, so more than 1 means it hit another bubble
             self.handle_collision(collided_bubbles)
 
     def handle_collision(self, collided_bubbles):
-        # Simple collision handling: remove the bubble or add other logic
-        for hit in collided_bubbles:
-            if hit != self.current_bubble:  # Avoid killing the bubble itself
-                if hit.color == self.current_bubble.color:
-                    hit.kill()  # Remove the collided bubble
+        # TODO: snap when it's close enough
+        self.snap()
+
+    def snap(self):
+        assert self.current_bubble
+        occupied = set()
+        for bubble in self.bubbles:
+            if bubble is self.current_bubble:
+                continue
+            occupied.add((bubble.x, bubble.y))
+
+        closest_coordinates = None
+        closest_distance = None
+        for cy in range(GRID_HEIGHT):
+            for cx in range(GRID_WIDTH):
+                x = get_center(cx)
+                y = get_center(cy)
+                if (x, y) in occupied:
+                    continue
+                distance = (self.current_bubble.x-x)**2 + (self.current_bubble.y-y)**2
+                if not closest_distance or closest_distance > distance:
+                    closest_coordinates = (x, y)
+                    closest_distance = distance
+        assert closest_distance, occupied
+        self.current_bubble.x, self.current_bubble.y = closest_coordinates
+        self.current_bubble.dx = 0
+        self.current_bubble.dy = 0
+        self.current_bubble = None
+        self.traverse(closest_coordinates)
+
+    def traverse(self, point):
+        # build grid
+        position_to_bubble = {}
+        for v in self.bubbles:
+            position_to_bubble[(v.x, v.y)] = v
+        grid_bubbles = {}
+        start_cell = None
+        for cy in range(GRID_HEIGHT):
+            for cx in range(GRID_WIDTH):
+                x = get_center(cx)
+                y = get_center(cy)
+                if (x, y) == point:
+                    start_cell = (cx, cy)
+                grid_bubbles[(cx, cy)] =  position_to_bubble.get((x, y))
+        # traverse starting from point (start_cell)
+        has_color_match = False
+        starting_bubble = grid_bubbles[start_cell]
+        for cell in neigbour_cells(start_cell):
+            bubble = grid_bubbles.get(cell)
+            if not bubble:
+                continue
+            if bubble.color == starting_bubble.color:
+                has_color_match = True
+                break
+        # if adjustent balls contains same color remove all such colors.
+        if not has_color_match:
+            return
+        cells = [start_cell]
+        seen = set()
+        while cells:
+            cell = cells.pop(0)
+            seen.add(cell)
+            bubble = grid_bubbles[cell]
+            bubble.kill()
+            for next_cell in neigbour_cells(cell):
+                if next_cell in seen:
+                    continue
+                next_bubble = grid_bubbles.get(next_cell)
+                if not next_bubble:
+                    continue
+                if next_bubble.color != bubble.color:
+                    continue
+                cells.append(next_cell)
+
 
     def check_state(self):
         # Current bubble become part of the board or destroyed
@@ -127,8 +218,8 @@ class Bubble(pygame.sprite.Sprite):
     def update(self):
         self.x = 1.0*self.x + self.dx
         self.y = 1.0*self.y + self.dy
-        self.rect.x = self.x
-        self.rect.y = self.y
+        self.rect.x = self.x - BUBBLE_SIZE//2
+        self.rect.y = self.y - BUBBLE_SIZE//2
         if self.rect.right >= SCREEN_WIDTH or self.rect.left <= 0:
             self.dx = -self.dx
         if self.rect.top <= 0:
